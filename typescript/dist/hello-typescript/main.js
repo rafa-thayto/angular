431dd48b75893d260bc4b45b569de9f09965c44d
Rafael Thayto <rafa.thayto@gmail.com>
2018-01-14 12:49:47 -0200

    Added the app1 and updated the organization of the folders

---
+++ typescript/dist/hello-typescript/main.js
@@ -0,0 +1,24 @@
+"use strict";
+var minhaVar = 'minha variavel';
+function minhaFunc(x, y) {
+    return x + y;
+}
+// ES 6 ou ES 2015
+var num = 2;
+var PI = 3.14;
+var numeros = [1, 2, 3]; // ES 5
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; }); // ES 6
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+// Tipagem de variáveis em typescript
+var n1 = 'askdjkas';
+n1 = 4;
