46fe4c8eef4158228feef7a17e76f3e2b2eac8c6
Rafael Thayto <rafa.thayto@gmail.com>
2018-01-15 18:49:30 -0200

    Update the progress bar

--- projects/app1/src/app/painel/frases-mock.ts
+++ projects/app1/src/app/painel/frases-mock.ts
@@ -3,6 +3,7 @@ import { Frase } from '../shared/frase.model';
 export const FRASES: Frase[] = [
     { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' },
     { fraseEng: 'I watch tv', frasePtBr: 'Eu assisto tv' },
-    { fraseEng: 'How are you', frasePtBr: 'Como vai você?' },
-    { fraseEng: 'I eat potato', frasePtBr: 'Eu como batata' }
+    { fraseEng: 'How are you?', frasePtBr: 'Como está você?' },
+    { fraseEng: 'I eat potato', frasePtBr: 'Eu como batata' },
+    { fraseEng: 'I love you', frasePtBr: 'Eu amo você'}
 ];
--- projects/app1/src/app/painel/painel.component.ts
+++ projects/app1/src/app/painel/painel.component.ts
@@ -11,12 +11,48 @@ import { FRASES } from './frases-mock';
 export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES;
-
   public instrucao: string = 'Traduza a frase:';
+  public resposta: string;
 
-  constructor() { console.log(this.frases); }
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
+
+  public progresso: number = 0;
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.rodadaFrase);
+  }
 
   ngOnInit() {
+  }
+
+  public atualizaResposta(resposta: Event): void {
+    // Pega o valor dentro do textarea
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+  }
+
+  public verificarResposta(): void {
+
+    if (this.rodadaFrase.frasePtBr === this.resposta) {
+      alert('A tradução está correta');
+
+
+      // Trocar pergunta da rodada e acrescenta progresso
+      if ((this.rodada + 1 ) < this.frases.length) {
+
+        // Progresso
+        this.progresso += (100 / this.frases.length);
+
+        // Troca pergunta
+        this.rodadaFrase = this.frases[++this.rodada];
+
+      }
+
+    } else {
+      alert('A tradução está errada');
+    }
+
   }
 
 }
